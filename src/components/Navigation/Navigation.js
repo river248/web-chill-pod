577266c2cd41a8b4da70b17dab19fcc904fd4b6a
river248 <85404917+river248@users.noreply.github.com>
2021-09-15 21:57:58 +0700

    Initial project.

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navigation.scss'
+
+function Navigation() {
+    return (
+        <div className="navigation">
+            This is Naigation
+        </div>
+    )
+}
+
+export default Navigation
