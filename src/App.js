5c2c7b898fd2843ea167f50ff7d031d3c2f29e9a
river248 <85404917+river248@users.noreply.github.com>
2021-09-19 01:18:13 +0700

    Initial home page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom"
+
+import Header from 'components/Header/Header'
 import Navigation from 'components/Navigation/Navigation'
+import ToTop from 'components/ToTop/ToTop'
 import './App.scss'
+import Home from "components/Home/Home"
+import ProductSection from "components/ProductSection/ProductSection"
+import ScrollToTop from "utils/ScrollToTop"
 
 function App() {
   return (
-    <div className="app">
-      Hello World!
-      <Navigation/>
-    </div>
+    <Router>
+      <div className="app">
+
+        <ScrollToTop>
+          <Switch>
+            <Route exact path="/">
+              <Navigation/>
+              <Home/>
+              <ProductSection/>
+            </Route>
+          </Switch>
+        </ScrollToTop>
+
+        <ToTop/>
+      </div>
+    </Router>
   )
 }
 
